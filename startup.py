dfaea3a591ee9d8c12947971ed26ed7ab42ceba6
rogetsun <songyw163@163.com>
2016-09-08 15:16:25 +0800

    commit

--- startup.py
+++ startup.py
@@ -3,7 +3,9 @@ from tornado.ioloop import IOLoop
 from app import app
 import tornado
 
-print "tornado version:%s" % (tornado.version,)
+from server.log_config import logger
+
+logger.debug("tornado version:%s" % (tornado.version,))
 
 app.listen(8000)
 IOLoop.current().start()
