2e44e4368340267de98b38d9196373412526eccb
rogetsun <songyw163@163.com>
2016-09-08 14:39:04 +0800

    commit

--- server/db/dao/file_dao.py
+++ server/db/dao/file_dao.py
@@ -15,6 +15,7 @@ def get_files(project_id):
 
 
 def save_file(file_dict, db):
+    """根据文件file_path_name, ft_id, project_id创建或更新文件"""
     f = get_file(file_dict.get('project_id'), file_dict.get('ft_id'), file_dict.get('file_path_name'), db)
     if f:
         file_dict['file_id'] = f.get('file_id')
@@ -43,6 +44,7 @@ def save_file(file_dict, db):
 
 
 def get_file(project_id, ft_id, file_path_name, db):
+    """根据项目、文件类型和文件路径名称查询文件"""
     f = db.getOne("""select * from pm.d_file where project_id=%s and  ft_id=%s and file_path_name=%s""",
                   (project_id, ft_id, file_path_name))
     return f
@@ -55,3 +57,22 @@ def get_file_his(file_id):
         (file_id,))
     db.dispose()
     return r
+
+
+def update_file_version(file_id, opr, db=Mysql()):
+    """更新文件版本号和当前版本号创建工号,并插入历史表"""
+    opr['file_id'] = file_id
+    db.update("""update pm.d_file
+    set curr_version = curr_version+1, create_time=%(op_time)s, create_login_id=%(login_id)s
+    where file_id=%(file_id)s"""
+              , opr)
+    db.insertOne("""
+    insert into pm.d_file_his(
+      file_id, project_id, file_path_name, version, file_desc, ft_id, create_time, create_login_id
+      )
+      select file_id, project_id, file_path_name, curr_version, file_desc, ft_id, create_time, create_login_id
+      from pm.d_file where file_id=%s
+    """, (file_id,))
+    f = db.getOne("""select * from pm.d_file where file_id=%s""", (file_id,))
+    logger.debug("更新文件%s完成,新版本号:%s" % (f.get('file_path_name'), f.get("curr_version")))
+    return f
