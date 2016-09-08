7217241cfcc9203cfa3b6d0514c694088c96461f
rogetsun <songyw163@163.com>
2016-09-08 15:15:08 +0800

    commit

--- startup.py
+++ startup.py
@@ -3,7 +3,7 @@ from tornado.ioloop import IOLoop
 from app import app
 import tornado
 
-print tornado.version
+print "tornado version:%s" % (tornado.version,)
 
 app.listen(8000)
 IOLoop.current().start()
