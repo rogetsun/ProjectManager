a187cf6160449b7ae4bc96e8c84cf55a48964ab9
rogetsun <songyw163@163.com>
2016-09-09 11:57:02 +0800

    commit

--- startup.py
+++ startup.py
@@ -2,6 +2,7 @@
 from tornado.ioloop import IOLoop
 from app import app
 import tornado
+import sys
 
 from server.log_config import logger
 
@@ -11,5 +12,11 @@ setting = {
     "max_buffer_size": 204800000  # 200MB
 }
 
-app.listen(8000, address="0.0.0.0", **setting)
-IOLoop.current().start()
+if __name__ == "__main__":
+    port = sys.argv.__len__() >= 2 and sys.argv[1] or 2204
+    port = int(port)
+    ip = sys.argv.__len__() >= 3 and sys.argv[2] or "0.0.0.0"
+
+    app.listen(port, address=ip, **setting)
+    logger.debug("app.listen %s:%s" % (ip, port))
+    IOLoop.current().start()
